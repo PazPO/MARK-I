1e6f34af2989a544e4f4c31e016d80fabd778daf
nikolasmelui <slak@samaradom.ru>
2019-11-29 21:03:47 +0400

    Add new data in secret.example.js file

--- secret.example.js
+++ secret.example.js
@@ -1,7 +1,13 @@
-module.export = {
-  host: 'localhost',
-  port: 5432,
-  database: 'database',
-  user: 'username',
-  password: 'password',
+module.exports = {
+  serverConfig: {
+    host: 'localhost',
+    port: 3000,
+  },
+  postgresConfig: {
+    host: 'localhost',
+    port: 5432,
+    database: 'database',
+    user: 'username',
+    password: 'password',
+  },
 };
